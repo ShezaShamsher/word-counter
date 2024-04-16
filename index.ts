acc6d6a61775efbbb992c91decb062536f471d96
Sheza Shamsher <160360815+ShezaShamsher@users.noreply.github.com>
2024-04-16 12:50:35 -0700

    sentence word counter

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer" ;
+
+import chalk from "chalk" ;
+
+const answers : {
+    sentence : string
+} = await inquirer.prompt ([
+    {
+     name : "sentence",
+     type : "input",
+     message : chalk.green.italic("Write Your Sentence To Count The Word")
+    }
+])
+
+let words = answers.sentence.trim().split(" ")
+
+console.log(words);
+ 
+console.log (chalk.yellowBright.bold(`\n Your Sentence Word Count Is ${words.length} \n `));
